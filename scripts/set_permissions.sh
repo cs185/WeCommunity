cd1e40e29387e712a2586499d5a449c35623781d
cs185 <cs185@rice.edu>
2024-11-30 22:25:58 -0600

    create log dir and give permission

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -2,3 +2,15 @@
 
 # Ensure JAR file has the correct permissions
 chmod 755 /home/ec2-user/wecommunity/wecommunity-0.0.1-SNAPSHOT.jar
+
+LOG_DIR="/var/log/community"
+
+if [ ! -d "$LOG_DIR" ]; then
+  echo "Creating log directory: $LOG_DIR"
+  mkdir -p $LOG_DIR
+fi
+
+echo "Ensuring proper ownership and permissions"
+chown ec2-user:ec2-user $LOG_DIR
+chmod 755 $LOG_DIR
+
