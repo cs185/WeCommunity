6edc3dc34d81cf09c4e7914393d0a7ca437b4a6f
cs185 <cs185@rice.edu>
2024-12-21 22:34:26 -0600

    remove es

--- src/main/java/edu/rice/wecommunity/dao/elasticsearch/DiscussPostRepository.java
+++ src/main/java/edu/rice/wecommunity/dao/elasticsearch/DiscussPostRepository.java
@@ -1,10 +1,10 @@
-package edu.rice.wecommunity.dao.elasticsearch;
-
-import edu.rice.wecommunity.entity.DiscussPost;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
-
-}
+//package edu.rice.wecommunity.dao.elasticsearch;
+//
+//import edu.rice.wecommunity.entity.DiscussPost;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
+//
+//}
