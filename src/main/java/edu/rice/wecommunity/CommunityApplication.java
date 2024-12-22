08bb304a59a24ab055814a9e6993356c3ec7a6fc
cs185 <cs185@rice.edu>
2024-12-21 21:02:33 -0600

    test without es config

--- src/main/java/edu/rice/wecommunity/CommunityApplication.java
+++ src/main/java/edu/rice/wecommunity/CommunityApplication.java
@@ -1,24 +1,24 @@
-//package edu.rice.wecommunity;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//import javax.annotation.PostConstruct;
-//
-//@SpringBootApplication
-//public class CommunityApplication {
-//
-//    @PostConstruct
-//    public void init() {
-//        // 解决netty启动冲突问题
-//        // see Netty4Utils.setAvailableProcessors()
-//        System.setProperty("es.set.netty.runtime.available.processors", "false");
-//    }
-//
-//    public static void main(String[] args) {
-////        String version = System.getProperty("java.version");
-////        System.out.println("Java Runtime Version: " + version);
-//        SpringApplication.run(CommunityApplication.class, args);
-//    }
-//
-//}
+package edu.rice.wecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    @PostConstruct
+    public void init() {
+        // 解决netty启动冲突问题
+        // see Netty4Utils.setAvailableProcessors()
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+    }
+
+    public static void main(String[] args) {
+//        String version = System.getProperty("java.version");
+//        System.out.println("Java Runtime Version: " + version);
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
